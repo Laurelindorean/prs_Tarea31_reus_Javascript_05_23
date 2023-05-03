3ade2e062015e21671fd41cc9dcf66cd2aecb521
Palmira Romia <kguinera@hotmail.com>
2023-05-04 00:51:38 +0200

    Task 13 complete

---
+++ Ejercicio13/js/script.js
@@ -0,0 +1,9 @@
+parrafo1 = x => alert("Se ha presionado el párrafo 1");
+
+parrafo2 = x => alert("Se ha presionado el párrafo 2");
+
+parrafo3 = x => alert("Se ha presionado el párrafo 3");
+
+parrafo4 = x => alert("Se ha presionado el párrafo 4");
+
+parrafo5 = x => alert("Se ha presionado el párrafo 5");
